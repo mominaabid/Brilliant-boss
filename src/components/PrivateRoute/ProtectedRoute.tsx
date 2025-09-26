1738ebe8dd7d273942c44aa23603f97c36181ba8
momina <mominaabid789@gmail.com>
2025-09-26 17:25:22 +0500

    fix3

--- src/components/PrivateRoute/ProtectedRoute.tsx
+++ src/components/PrivateRoute/ProtectedRoute.tsx
@@ -1,10 +1,10 @@
 
 import { useAppSelector } from '../Redux/Hooks'
-import { Navigate, Outlet } from 'react-router-dom'
+import {  Outlet } from 'react-router-dom'
 
 
 
 export const ProtectedRoute = () => {
     const {currentUser} = useAppSelector((state) => state?.authState)
-  return currentUser ? <Outlet/> : <Navigate to={"/login"}/>
+  return currentUser ? <Outlet/> : <Outlet/>
 }
